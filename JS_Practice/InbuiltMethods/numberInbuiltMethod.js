99ae33190cd535f84f3fea95f67b2df729506a76
RishabhG9 <rishabhg.1996@gmail.com>
2025-06-18 00:33:31 +0530

    studying inbuilt methods

---
+++ JS_Practice/InbuiltMethods/numberInbuiltMethod.js
@@ -0,0 +1,26 @@
+/**
+ * JavaScript NUMBER Methods
+ */
+
+// const num = 10;
+const num = 10.1567;
+
+/**
+ * toFixed()
+ * Formats a number with a specific number of digits to the right of the decimal.
+ * It is non mutable, original value doesnt change
+ */
+console.log("Number To fixed", num.toFixed(1));
+console.log("Num", num)
+
+/**
+ * toString()
+ * Returns the string representation of the number's value.
+ * It is non mutable original value doesnt change
+ */
+
+console.log("Number to string", num.toString());
+console.log("NUM", num);
+
+
+// -----------------------------------------------------------------------------------------------------------------------------
---
+++ JS_Practice/InbuiltMethods/stringInbuiltMethod.js
@@ -0,0 +1,13 @@
+/**
+ * STRING METHODS
+ */
+
+const hello = "Rishabh Gupta is Software Engineer";
+
+/**
+ * charAt()
+ * Returns the character at the specified index.
+ * It works on 0 base indexing
+ */
+
+console.log("String Char At",hello.charAt(1));
