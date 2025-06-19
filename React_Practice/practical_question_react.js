7e0769e995beb25bc51a2c8eeef5976cfdf8073a
RishabhG9 <rishabhg.1996@gmail.com>
2025-06-19 09:31:30 +0530

    added new file for array methods

--- React_Practice/practical_question_react.js
+++ React_Practice/practical_question_react.js
@@ -1,11 +1,18 @@
 /**
+ * 
  * React Practical questions
  * Debouncer
  * Common Forms
  * Infinite Scrolling
  * Stopwatch
+ * Accordion (CUSTOM) and Nested Accordion
  * Login and Signup Page
+ * Mobile responsiveness
+ * To Do Page (Add Favourite, Remove Favourite)
+ * Filter (like Sorting Price (Low & high))
  * Redux and State handling
  * Loading state and Error handling
+ * Rendering (useMemo, useCallback, React.memo)   **IMPORTANT
+ * Parent and Child Component prop passing and rendering **IMPORTANT
  * 
  */
